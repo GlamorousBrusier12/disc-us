1e2de1f43a40eb298dcc30cccb13707bdc341b51
Naveen <pandurebelsachin18@gmail.com>
2021-12-27 21:46:05 +0530

    added router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchPosts } from "../actions/posts";
-import { PostList } from "./index";
+import { PostList } from "./";
 import PropTypes from "prop-types";
+import { Navbar } from "./";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+const Home = (props) => {
+  console.log("Props: ", props);
+  return <h1>Home</h1>;
+};
+const Login = () => <h1>Login</h1>;
+const SignUp = () => <h1>SignUp</h1>;
+
 class App extends Component {
   componentDidMount() {
     this.props.dispatch(fetchPosts());
@@ -12,60 +22,22 @@ class App extends Component {
     // console.log("Props: ", this.props);
     const { posts } = this.props;
     return (
-      <div>
-        <nav className="nav">
-          <div className="left-div">
-            <h1 className="website-name">{"< Disc-Us/>"}</h1>
-            {/* <img src="https://cdn-icons-png.flaticon.com/512/1205/1205495.png" /> */}
-          </div>
-          <div className="search-container">
-            <img
-              className="search-icon"
-              src="https://cdn-icons-png.flaticon.com/512/54/54481.png"
-              alt="search-icon"
-            />
-            <input placeholder="Search" />
-
-            <div className="search-results">
-              <ul>
-                <li className="search-results-row">
-                  <img
-                    src="https://cdn-icons.flaticon.com/png/512/2202/premium/2202112.png?token=exp=1637769738~hmac=22e534c0a6c3367f991a51f75af0c1d0"
-                    alt="user-dp"
-                  />
-                  <span>John Doe</span>
-                </li>
-                <li className="search-results-row">
-                  <img
-                    src="https://cdn-icons.flaticon.com/png/512/2202/premium/2202112.png?token=exp=1637769738~hmac=22e534c0a6c3367f991a51f75af0c1d0"
-                    alt="user-dp"
-                  />
-                  <span>John Doe</span>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="right-nav">
-            <div className="user">
-              <img
-                src="https://cdn-icons-png.flaticon.com/512/449/449586.png"
-                alt="user-dp"
-                id="user-dp"
-              />
-              <span>John Doe</span>
-            </div>
-            <div className="nav-links">
-              <ul>
-                <li>Log in</li>
-                <li>Log out</li>
-                <li>Register</li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-
-        <PostList posts={posts} />
-      </div>
+      <Router>
+        <div>
+          <Navbar />
+          {/* <PostList posts={posts} /> */}
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+          </ul>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        </div>
+      </Router>
     );
   }
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
 import App from "./App";
 import PostList from "./PostList";
-
-export { App, PostList };
+import Navbar from "./Navbar";
+export { App, PostList, Navbar };
